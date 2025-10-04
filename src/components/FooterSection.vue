4773695b02b35960e5b868320864f7004188f12d
matttccc <hi@mattt.cc>
2025-10-05 00:27:10 +0100

    footer and links

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -19,12 +19,12 @@
         <div class="footer-section">
           <h4 class="footer-heading">League Results</h4>
           <div class="league-links">
-            <a href="#" class="league-link">Season 2024/2025 Division One</a>
-            <a href="#" class="league-link">Season 2024/2025 Division Two</a>
-            <a href="#" class="league-link">Season 2024/2025 Women's Division</a>
+            <a href="#" class="league-link">Season 2025/2026 (Current)</a>
+            <a href="#" class="league-link">Season 2024/2025</a>
             <a href="#" class="league-link">Season 2023/2024</a>
             <a href="#" class="league-link">Season 2022/2023</a>
             <a href="#" class="league-link">Season 2021/2022</a>
+            <a href="#" class="league-link void">Season 2020/2021</a>
             <a href="#" class="league-link">Season 2019/2020</a>
             <a href="#" class="league-link">Season 2018/2019</a>
             <a href="#" class="league-link">Season 2017/2018</a>
@@ -43,9 +43,9 @@
         <div class="footer-section">
           <h4 class="footer-heading">Downloads</h4>
           <div class="download-links">
-            <a href="#" class="download-link">Result Sheet</a>
-            <a href="#" class="download-link">NoCoVo League Rules</a>
-            <a href="#" class="download-link">NCVA Constitution</a>
+            <a href="/pdf/result-sheet.pdf" class="download-link" target="_blank">Result Sheet</a>
+            <a href="/pdf/northern-counties-league-rules-2025.pdf" class="download-link" target="_blank">NoCoVo League Rules</a>
+            <a href="/pdf/ncva-constitution.pdf" class="download-link" target="_blank">NCVA Constitution</a>
           </div>
         </div>
 
@@ -56,6 +56,20 @@
             <a href="https://www.volleyballengland.org/competitions/volleyball/nvl" class="external-link" target="_blank">National Volleyball League</a>
             <a href="https://www.volleyballengland.org/" class="external-link" target="_blank">Volleyball England</a>
             <a href="https://a.leaguerepublic.com/myaccount/login/index.html" class="external-link" target="_blank">NoCoVo League Admin</a>
+
+            <div class="social-links">
+              <a href="https://www.facebook.com/groups/188203511311097/" class="social-link" target="_blank" title="Join our group on Facebook">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="#494A53" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                </svg>
+              </a>
+
+              <a href="https://www.instagram.com/northern_counties_volleyball/" class="social-link" target="_blank" title="Follow us on Instagram">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="#494A53" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
 
@@ -159,51 +173,64 @@ import { RouterLink } from 'vue-router'
       font-weight: 500;
     }
 
-    a {
+    a,
+    span {
       color: var(--color-text-light);
       text-decoration: none;
       transition: color 0.2s;
       display: flex;
       flex-direction: column;
-      gap: 0.25rem;
       font-size: 14px;
 
       &:hover {
         color: var(--color-text);
       }
+      &.void {
+        opacity: 0.45;
+        pointer-events: none;
+      }
+    }
+
+    .clubs-list,
+    .league-links,
+    .download-links,
+    .external-links {
+      display: flex;
+      flex-direction: column;
+      gap: 2px;
     }
   }
 }
 
-.clubs-list {
+.social-links {
   display: flex;
-  flex-direction: column;
-  gap: 0.25rem;
-
-  span {
-    font-size: 0.85rem;
-    color: var(--color-text-light);
-  }
+  gap: 10px;
+  margin-top: 1rem;
 }
 
-// .league-links,
-// .download-links,
-// .external-links {
-//   display: inline-flex;
-//   flex-direction: column;
-//   gap: 0.25rem;
-// }
+.social-link {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
 
-// .league-link,
-// .download-link,
-// .external-link {
-//   color: var(--color-link);
-//   font-size: 0.85rem;
-//   transition: color 0.2s;
+  svg {
+    transition: transform 0.2s ease;
+    background: var(--color-background-light);
+    padding: 18px;
+    width: 56px;
+    height: 56px;
+    box-sizing: border-box;
+    border-radius: 8px;
+    transition: all 0.2s ease;
 
-//   &:hover {
-//     color: var(--color-link) !important;
-//   }
-// }
+    path {
+      fill: var(--color-text);
+    }
+  }
+
+  &:hover svg {
+    scale: 1.1;
+  }
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,6 +25,59 @@
 
     <div class="page-content container">
 
+      <!-- Download Cards Section -->
+      <div class="download-cards-section">
+        <div class="download-card">
+          <div class="download-icon">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M14 2H6C4.9 2 4 2.9 4 4V20C4 21.1 4.89 22 5.99 22H18C19.1 22 20 21.1 20 20V8L14 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M14 2V8H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M8 9H10.8" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M8 12H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M8 15H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M8 18H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+          </div>
+          <div class="download-content">
+            <h3>Result Sheet</h3>
+            <p>Download the official result sheet for recording match results</p>
+            <a href="/pdf/result-sheet.pdf" class="download-btn" target="_blank">
+              <span>Download PDF</span>
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M7 10L12 15L17 10" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12 15V3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <div class="download-card">
+          <div class="download-icon">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M16 4H18C18.5304 4 19.0391 4.21071 19.4142 4.58579C19.7893 4.96086 20 5.46957 20 6V20C20 20.5304 19.7893 21.0391 19.4142 21.4142C19.0391 21.7893 18.5304 22 18 22H6C5.46957 22 4.96086 21.7893 4.58579 21.4142C4.21071 21.0391 4 20.5304 4 20V6C4 5.46957 4.21071 4.96086 4.58579 4.58579C4.96086 4.21071 5.46957 4 6 4H8" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M15 2H9C8.44772 2 8 2.44772 8 3V5C8 5.55228 8.44772 6 9 6H15C15.5523 6 16 5.55228 16 5V3C16 2.44772 15.5523 2 15 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M8 9H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M8 12H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M8 15H14" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M8 18H12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+          </div>
+          <div class="download-content">
+            <h3>League Rules</h3>
+            <p>Official Northern Counties Volleyball League Rules for 2025/2026 season</p>
+            <a href="/pdf/northern-counties-league-rules-2025.pdf" class="download-btn" target="_blank">
+              <span>Download PDF</span>
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M7 10L12 15L17 10" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12 15V3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+
       <h2>Current season</h2>
 			<div class="home-tables">
 
@@ -32,7 +85,7 @@
           <h3 class="division-name"><a href="https://www.ncva.org.uk/league/division-one/">Division One</a></h3>
 
           <div class="results-table" id="lrep660431464">
-            <p class="temp">Data loading from LeagueRepublic...
+            <p class="temp">Loading data from LeagueRepublic...
               <br>
               <a href="https://ncva.leaguerepublic.com/fg/1_916473096.html" target="_blank">View on LeagueRepublic</a>
             </p>
@@ -48,7 +101,7 @@
           <h3 class="division-name"><a href="https://www.ncva.org.uk/league/division-two/">Division Two</a></h3>
 
           <div class="results-table" id="lrep189526043">
-            <p class="temp">Data loading from LeagueRepublic...
+            <p class="temp">Loading data from LeagueRepublic...
               <br>
               <a href="https://ncva.leaguerepublic.com/fg/1_490197339.html" target="_blank">View on LeagueRepublic</a>
             </p>
@@ -65,7 +118,7 @@
           <h3 class="division-name"><a href="https://www.ncva.org.uk/league/womens-division/">Women's Division</a></h3>
 
           <div class="results-table" id="lrep443795018">
-            <p class="temp">Data loading from LeagueRepublic...
+            <p class="temp">Loading data from LeagueRepublic...
               <br>
               <a href="https://ncva.leaguerepublic.com/fg/1_923905458.html" target="_blank">View on LeagueRepublic</a>
             </p>
@@ -161,6 +214,9 @@ onMounted(() => {
 </script>
 
 <style lang="scss">
+.hero-section {
+  padding-bottom: 120px;
+}
 .hero-wrapper {
   display: flex;
   flex-direction: column;
@@ -188,7 +244,7 @@ onMounted(() => {
   align-items: center;
   justify-content: center;
   background-color: var(--color-white);
-  border-radius: 10px;
+  border-radius: 12px;
   width: 20%;
   line-height: 1;
   color: var(--color-text);
@@ -284,6 +340,7 @@ onMounted(() => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  color: var(--color-text-light);
 
   a {
     text-decoration: none;
@@ -380,10 +437,10 @@ onMounted(() => {
 }
 .buttons a {
   display: inline-flex;
-  padding: 4px 10px;
+  padding: 6px 10px;
   margin-right: 5px;
   background: var(--color-background-light);
-  border-radius: 3px;
+  border-radius: 5px;
   width: 100%;
   justify-content: center;
   color: #222222;
@@ -409,5 +466,118 @@ onMounted(() => {
 .buttons a:hover {
   background-color: var(--color-white);
   transform: translateY(-2px);
+}
+
+/* Download Cards Section */
+.download-cards-section {
+  background: var(--color-white);
+  padding: 2rem;
+  margin: -120px 0 40px;
+  border-radius: 12px;
+  display: flex;
+  gap: 2rem;
+  flex-wrap: wrap;
+}
+
+.download-card {
+  flex: 1;
+  min-width: 300px;
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
+  padding: 20px;
+  background: var(--color-background-light);
+  border-radius: 8px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    box-shadow: 0 8px 25px rgba(0, 0, 0, 0.15);
+  }
+}
+
+.download-icon {
+  flex-shrink: 0;
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--color-white);
+  // background: var(--color-background);
+
+  svg {
+    scale: 1.75;
+    path {
+      stroke: var(--color-accent);
+      stroke-width: 1.2;
+    }
+  }
+}
+
+.download-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding-top: 14px;
+
+  h3 {
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 24px;
+    color: var(--color-text);
+    margin: 0;
+    line-height: 1.2;
+  }
+
+  p {
+    color: var(--color-text-light);
+    font-size: 0.9rem;
+    line-height: 1.4;
+    margin: 0;
+  }
+}
+
+.download-btn {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 16px 24px;
+  margin: 15px 0;
+  background: var(--color-link);
+  color: var(--color-white);
+  text-decoration: none;
+  border-radius: 6px;
+  font-weight: 600;
+  font-size: 0.9rem;
+  transition: all 0.3s ease;
+  align-self: flex-start;
+
+  &:hover {
+    background: var(--color-blue-dark);
+    box-shadow: 0 4px 12px rgba(65, 90, 205, 0.3);
+  }
+}
+
+@media (max-width: 768px) {
+  .download-cards-section {
+    flex-direction: column;
+    padding: 1.5rem;
+    margin: 1.5rem 0;
+  }
+
+  .download-card {
+    min-width: auto;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  .download-icon {
+    align-self: center;
+  }
+
+  .download-btn {
+    align-self: center;
+  }
 }
 </style>
